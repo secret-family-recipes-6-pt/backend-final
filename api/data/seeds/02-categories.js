5ed2c31e11d49aaf71f2977ea15520115fa307fc
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-07-26 20:19:23 -0400

    categories

--- api/data/seeds/02-categories.js
+++ api/data/seeds/02-categories.js
@@ -1,10 +1,10 @@
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex("table_name")
+  return knex("categories")
     .del()
     .then(function () {
       // Inserts seed entries
-      return knex("table_name").insert([
+      return knex("categories").insert([
         { category_name: "breakfast" },
         { category_name: "lunch" },
         { category_name: "dinner" },
