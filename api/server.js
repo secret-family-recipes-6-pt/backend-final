2a54a43e9f45bdc56626d84673d3412663331bfb
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-07-27 00:30:45 -0400

    removed restricted middleware temporarily

--- api/server.js
+++ api/server.js
@@ -20,10 +20,10 @@ server.use(cors());
 
 // ENDPOINTS
 server.use("/api/auth", AuthRouter);
-server.use("/api/users", restricted, UsersRouter);
-server.use("/api/recipes", restricted, RecipesRouter);
-server.use("/api/ing", restricted, IngredientsRouter);
-server.use("/api/inst", restricted, InstructionsRouter);
+server.use("/api/users", /*restricted,*/ UsersRouter);
+server.use("/api/recipes", /*restricted,*/ RecipesRouter);
+server.use("/api/ing", /*restricted,*/ IngredientsRouter);
+server.use("/api/inst", /*restricted,*/ InstructionsRouter);
 
 // CATCH ALL
 // server.use((err, req, res) => {
