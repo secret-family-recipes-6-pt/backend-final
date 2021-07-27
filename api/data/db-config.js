dfabe29ab718343f59ead38bba5353033a156ccf
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-07-26 22:10:52 -0400

    knexfile

--- api/data/db-config.js
+++ api/data/db-config.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 
 const knex = require("knex");
 const dbEnvironment = process.env.NODE_ENV || "development";
-const configs = require("../../knexfile")[dbEnvironment];
+const configs = require("../../knexfile.js")[dbEnvironment];
 
 module.exports = knex(configs[process.env.NODE_ENV]);
